c049a8007e12787f6317532b887fc5c7f18881c7
Dino Himaj <dinohimaj@rocketmail.com>
2023-01-09 23:54:13 +0100

    enemy movement almost finished

--- EnemyController.js
+++ EnemyController.js
@@ -1,4 +1,5 @@
 import Enemy from './Enemy.js';
+import MovingDirection from './MovingDirection.js';
 
 export default class EnemyController {
   enemyMap = [
@@ -12,17 +13,74 @@ export default class EnemyController {
 
   enemyRows = [];
 
+  currentDirection = MovingDirection.right;
+  xVelocity = 0;
+  yVelocity = 0;
+  defaultXVelocity = 1;
+  defaultYVelocity = 1;
+  moveDownTimerDefault = 30;
+  moveDownTimer = this.moveDownTimerDefault;
+
   constructor(canvas) {
     this.canvas = canvas;
     this.createEnemies();
   }
 
   draw(ctx) {
+    this.decrementMoveDownTimer();
+    this.updateVelocityAndDirection();
     this.drawEnemies(ctx);
+    this.resetMoveDownTimer();
+    console.log(this.moveDownTimer);
+  }
+
+  resetMoveDownTimer() {
+    if (this.moveDownTimer <= 0) {
+      this.moveDownTimer = this.moveDownTimerDefault;
+    }
+  }
+
+  decrementMoveDownTimer() {
+    if (
+      this.currentDirection === MovingDirection.downLeft ||
+      this.currentDirection === MovingDirection.downRight
+    ) {
+      this.moveDownTimer--;
+    }
+  }
+
+  updateVelocityAndDirection() {
+    for (const enemyRow of this.enemyRows) {
+      if (this.currentDirection == MovingDirection.right) {
+        this.xVelocity = this.defaultXVelocity;
+        this.yVelocity = 0;
+        const rightMostEnemy = enemyRow[enemyRow.length - 1];
+        //if most right eme,y hits the edge of the screen => move/change direction
+        if (rightMostEnemy.x + rightMostEnemy.width >= this.canvas.width) {
+          this.currentDirection = MovingDirection.downLeft;
+          break;
+        }
+      } else if (this.currentDirection === MovingDirection.downLeft) {
+        if (this.moveDown(MovingDirection.left)) {
+          break;
+        }
+      }
+    }
+  }
+
+  moveDown(newDirection) {
+    this.xVelocity = 0;
+    this.yVelocity = this.defaultXVelocity;
+    if (this.moveDownTimer <= 0) {
+      this.currentDirection = newDirection;
+      return true;
+    }
+    return false;
   }
 
   drawEnemies(ctx) {
     this.enemyRows.flat().forEach((enemy) => {
+      enemy.move(this.xVelocity, this.yVelocity);
       enemy.draw(ctx);
     });
   }
@@ -33,7 +91,7 @@ export default class EnemyController {
       row.forEach((enemyNubmer, enemyIndex) => {
         if (enemyNubmer > 0) {
           this.enemyRows[rowIndex].push(
-            new Enemy(enemyIndex * 50, rowIndex * 35, enemyNubmer)
+            new Enemy(enemyIndex * 50, rowIndex * 35, enemyNubmer) // 50 and 35 = gap between
           );
         }
       });
